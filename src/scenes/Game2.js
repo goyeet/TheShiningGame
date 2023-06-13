e8feea1ec515d9a76073421daa6ff8a22913c9f0
“Emily <egavilan@ucsc.edu>
2023-06-12 22:01:47 -0700

    game 2 changes

--- src/scenes/Game2.js
+++ src/scenes/Game2.js
@@ -6,8 +6,8 @@ class Game2 extends Phaser.Scene {
     preload() {
         this.load.path = './assets/';
         
-        // this.load.image('tileset2Image', 'tilesets/PLACEHOLDER.png');
-        // this.load.tilemapTiledJSON('tilemap2JSON', 'tilemaps/Game2.json');
+        this.load.image('tileset2Image', 'tilesets/BallroomBackground.png');
+        this.load.tilemapTiledJSON('tilemap2JSON', 'tilemaps/Game2.json');
 
         this.load.image('Jack', 'characters/blackBox.png'); // Placeholder sprite
         this.load.image('Delbert', 'characters/redBox.png'); // Placeholder sprite
@@ -29,25 +29,24 @@ class Game2 extends Phaser.Scene {
         this.cameras.main.setBackgroundColor('#202020');
 
         // Tilemap setup
-        // const map = this.add.tilemap('tilemap2JSON');
-        // const tileset = map.addTilesetImage('PLACEHOLDERTILESETNAME', 'tileset2Image');
-        // const bgLayer = map.createLayer('backgroundLayer', tileset, 0, 0);
-        // const terrainLayer = map.createLayer('terrainLayer', tileset, 0, 0);
-        // terrainLayer.setCollisionByProperty({ collides: true });
+        const map = this.add.tilemap('tilemap2JSON');
+        const tileset = map.addTilesetImage('BallroomBackground', 'tileset2Image');
+        const terrainLayer = map.createLayer('collisionLayer', tileset, 0, 0);
+        terrainLayer.setCollisionByProperty({ collides: true });
         
         // Instantiate Jack
-        this.Jack = new Player(this, game.config.width/2, game.config.width/2, 'Jack');
+        this.Jack = new Player(this, tileSize*26, tileSize*3, 'Jack').setScale(0.5).setOrigin(0,0);
 
         // Add collider between Jack and tilemap
-        // this.physics.add.collider(this.Jack, terrainLayer);
+        this.physics.add.collider(this.Jack, terrainLayer);
 
         // Have camera follow Jack
-        // this.cameras.main.startFollow(this.Jack, true, 0.25, 0.25);
+        this.cameras.main.startFollow(this.Jack, true, 0.25, 0.25);
 
         this.cameras.main.setBounds(0, 0, game.config.width, game.config.height);
 
         // Set world bounds to map dimensions
-        this.physics.world.bounds.setTo(0, 0, game.config.width/* map.widthInPixels */, game.config.height/* map.heightInPixels */);
+        this.physics.world.bounds.setTo(0, 0, map.widthInPixels, map.heightInPixels);
 
         // start bg music
         /* this.bgMusic = this.sound.add('bgMusic', { volume: 0.65, loop: true });
@@ -56,38 +55,36 @@ class Game2 extends Phaser.Scene {
             bgMusicPlaying = true;
         } */
 
-        /* this.enemyGroup = this.add.group({
-            runChildUpdate: true    // make sure update runs on group children
-        });
- */
         this.enemyGroup = this.physics.add.group({
             immovable: true,
             runChildUpdate: true    // make sure update runs on group children
         });
 
+        // Add collider between Jack and tilemap
+        this.physics.add.collider(this.enemyGroup, terrainLayer);
+
         // TEMP
-        this.guest = new Enemy(this, 100, 100, 'Jack');
-        this.guest2 = new Enemy(this, 800, 100, 'Jack');
+        this.guest = new Enemy(this, 100, 100, 'Jack').setScale(0.5);
+        this.guest2 = new Enemy(this, 600, 100, 'Jack').setScale(0.5);
         this.enemyGroup.add(this.guest);
         this.enemyGroup.add(this.guest2);
         /////////////////
 
-        this.Delbert = new Enemy(this, 400, 300, 'Delbert');
+        this.Delbert = new Enemy(this, tileSize*2, tileSize*6, 'Delbert').setOrigin(0,0);
 
         
         
 
-        // Randomly pick quest skin
-        const guestSkins = ['guest1', 'guest1', '']
+        // Randomly pick guest skin
+        // const guestSkins = ['guest1', 'guest2', 'guest3']
 
         // spawn in enemies on map
-        this.map.findObject('objectLayer', obj => {
-            if (obj.name.includes('enemySpawn')) {
-                this.guest = new Enemy(this, obj.x, obj.y, '');
-                this.enemyGroup.add(this.guest);
-            }
-        });
-        
+        // this.map.findObject('spawnLayer', obj => {
+        //     if (obj.name.includes('Spawn')) {
+        //         this.guest = new Enemy(this, obj.x, obj.y, /* Change to random skin**/'Delbert').setScale(0.5);
+        //         this.enemyGroup.add(this.guest);
+        //     }
+        // });
 
         // set up cursor keys
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
@@ -101,16 +98,21 @@ class Game2 extends Phaser.Scene {
     // logic that executes when enemy collides with regular guests
     enemyCollision() {
         console.log('collided with guest')
+        // fade in from black
+        this.cameras.main.fadeIn(2000, 0, 0, 0);
+        // TODO
         // spawn Jack back at the bar
-        this.Jack.setPosition(tileSize * 8, tileSize * 5); // bar's x, bar's y
+        this.Jack.setPosition(tileSize*26, tileSize*3); // bar's x, bar's y
     }
 
     gameOver() {
         console.log('collided with Delbert')
         this.gameOverFlag = true;
         this.Jack.setVelocity(0,0);
+        // fade in from black
+        this.cameras.main.fadeIn(4000, 0, 0, 0);
         // display game over screen
-        // 
+        // TODO
     }
 
     update() {
@@ -118,7 +120,7 @@ class Game2 extends Phaser.Scene {
             this.Jack.update();
 
             this.physics.world.overlap(this.Jack, this.enemyGroup, this.enemyCollision, null, this);
-            this.physics.world.overlap(this.Jack, this.Delbert, this.gameOver, null, this);
+            this.physics.world.overlap(this.Jack, this.Delbert, this.gameOver, null, this); // when player overlaps with delbert
         }
         
 
@@ -129,4 +131,4 @@ class Game2 extends Phaser.Scene {
     }
     
     
-}
+}  
